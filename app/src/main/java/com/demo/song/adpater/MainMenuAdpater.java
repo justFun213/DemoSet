fc388b4a187a3051b9bf9136ec29bb3b1e1e966e
song <gensongjob@163.com>
2018-05-13 02:43:30 +0800

    测试在dev修改

--- app/src/main/java/com/demo/song/adpater/MainMenuAdpater.java
+++ app/src/main/java/com/demo/song/adpater/MainMenuAdpater.java
@@ -44,7 +44,7 @@ public class MainMenuAdpater extends RecyclerView.Adapter<Menu> {
         mList.add("MyDrawableListActivity");
         mList.add("MyServiceListActivity");
         mList.add("MyAnimateListActivity");
-        //无聊测试一下
+        //无聊测试一下 哈哈  你是不是傻
         mList.add("MyFrameListActivity");
     }
 
