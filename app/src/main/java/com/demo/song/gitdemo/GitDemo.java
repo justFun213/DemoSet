53c1f5036b34c679488b38e86ec6b579529b1280
song <gensongjob@163.com>
2018-05-21 01:16:39 +0800

    添加webview练习7次

--- app/src/main/java/com/demo/song/gitdemo/GitDemo.java
+++ app/src/main/java/com/demo/song/gitdemo/GitDemo.java
@@ -7,7 +7,7 @@ public class GitDemo {
     public static void print(){
         Log.d(TAG,"单纯打印测一下大富豪doif和o");
         Log.d(TAG,"");
-        Log.d(TAG,"你s发生地fr我第三方sd啊？？");
-        Log.d(TAG,"小米mix2s和三星S8+哪个好");
+        Log.d(TAG,"你s阿森大厦发生大幅度释放公司共同用途啊？？");
+        Log.d(TAG,"小个好");
     }
 }
