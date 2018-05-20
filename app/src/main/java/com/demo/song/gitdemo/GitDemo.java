a0105d3db785f390ba8a049ff0715ab56c080ee4
song <gensongjob@163.com>
2018-05-21 00:39:28 +0800

    添加git练习

---
+++ app/src/main/java/com/demo/song/gitdemo/GitDemo.java
@@ -0,0 +1,11 @@
+package com.demo.song.gitdemo;
+
+import android.util.Log;
+
+public class GitDemo {
+    public static final String TAG="com.demo.song.gitdemo";
+    public static void print(){
+        Log.d(TAG,"单纯打印测一下大富豪doif和o");
+        Log.d(TAG,"dsfsdfsdfretergeggerergo");
+    }
+}
