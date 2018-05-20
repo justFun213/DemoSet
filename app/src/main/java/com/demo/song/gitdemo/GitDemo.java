78a6315a1a98e281c435f39e387cd8f4f79291ca
song <gensongjob@163.com>
2018-05-21 01:00:11 +0800

    添加git练习4次

--- app/src/main/java/com/demo/song/gitdemo/GitDemo.java
+++ app/src/main/java/com/demo/song/gitdemo/GitDemo.java
@@ -6,7 +6,8 @@ public class GitDemo {
     public static final String TAG="com.demo.song.gitdemo";
     public static void print(){
         Log.d(TAG,"单纯打印测一下大富豪doif和o");
-        Log.d(TAG,"dsfsdfsdfretdsfsdfsdergeggerergo");
-        Log.d(TAG,"你srfsr好sdfsfsrfr我是srf谁啊？？");
+        Log.d(TAG,"dsfs但是复苏的复苏的sretdsfsdfsdergeggerergo");
+        Log.d(TAG,"你srfsrsdfjiosdjfsf？？");
+        Log.d(TAG,"小米mix2s和三星S8+哪个好");
     }
 }
